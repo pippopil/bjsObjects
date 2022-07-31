615c3a13adaf23840b5b88ad583120e617b15bfc
pippopil <voood234@gmail.com>
2022-07-31 08:35:20 +0300

    Add

---
+++ task.js
@@ -0,0 +1,36 @@
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+}
+
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+}
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  for (let i = 0; i < marks.length; i++) {
+    this.addMark(marks[i]);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  } return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
